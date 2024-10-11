19b7e58dc1ffc9efdf2ada645e50f0e65d878d78
Asta(아스타) <86705803+P-Asta@users.noreply.github.com>
2024-10-11 21:00:27 +0900

    AC: 6278

--- 6278.rs
+++ 6278.rs
@@ -1,46 +1,33 @@
-// NOT AC
+// https://www.acmicpc.net/problem/6278
 use std::io::BufRead;
 
 fn main() {
     let mut buf = String::new();
     let n = input().parse::<u8>().unwrap();
     for i in 1..=n {
-        buf.push_str(&format!("Case #{i}: {}\n", calc()));
+        buf.push_str(&format!("Case #{i}: {}\n", calc(n)));
     }
     print!("{buf}");
 }
 
-fn calc() -> isize {
+fn calc(n: u8) -> usize {
     let i: Vec<_> = input()
         .split_whitespace()
-        .map(|x| x.parse::<isize>().unwrap())
+        .map(|x| x.parse::<usize>().unwrap())
         .collect();
+    let bag = i[0];
+    let (s1, v1, s2, v2) = (i[1], i[2], i[3], i[4]);
 
     let mut res = 0;
-    let mut bag = i[0];
+    for i in 0..(100_000_000 / n as usize).min(bag / s1) + 1 {
+        let remain = bag - s1 * i;
+        res = res.max(v1 * i + v2 * (remain / s2));
+    }
 
-    // 값 / 크기 를 하여 크기가 1일때의 가치를 구한다.
-    let emerald = i[2] as f32 / i[1] as f32;
-    let sapphires = i[4] as f32 / i[3] as f32;
-
-    // 크기가 1일때의 가치를 비교하여 가치가 높은 순으로 정렬한다.
-    let size_value = if emerald > sapphires {
-        [(i[1], i[2]), (i[3], i[4])]
-    } else if emerald == sapphires {
-        if bag / i[2] > bag / i[4] {
-            [(i[1], i[2]), (i[3], i[4])]
-        } else {
-            [(i[3], i[4]), (i[1], i[2])]
-        }
-    } else {
-        [(i[3], i[4]), (i[1], i[2])]
-    };
-    println!("{:?}", size_value);
-
-    // 크기가 큰 순서대로 가방에 담는다.
-    res += size_value[0].1 * (bag / size_value[0].0);
-    bag = bag % size_value[0].0;
-    res += size_value[1].1 * (bag / size_value[1].0);
+    for i in 0..(100_000_000 / n as usize).min(bag / s2) + 1 {
+        let remain = bag - s2 * i;
+        res = res.max(v2 * i + v1 * (remain / s1));
+    }
 
     res
 }
@@ -55,3 +42,4 @@ fn input() -> String {
         .trim()
         .to_string()
 }
+
